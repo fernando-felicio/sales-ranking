f3a6d2c20ec7e2762a683ac892249903c8962087
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-12-20 11:46:17 -0300

    Regras de negocio

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module sales_ranking {
+}
---
+++ src/sales_ranking/Main.java
@@ -0,0 +1,47 @@
+package sales_ranking;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int alcohol = 0;
+		int gasoline = 0;
+		int diesel = 0;
+		
+		int productCode = 0;
+		
+		System.out.printf("Enter the product code:%n 1 - Alcool%n 2 - Gasoline%n 3 - Diesel%n");
+		
+		while (productCode != 4) {
+			productCode = sc.nextInt();
+			
+			switch (productCode) {
+			
+			case 1:
+				alcohol += 1;
+				break;
+			case 2:
+				gasoline += 1;
+				break;
+			case 3:
+				diesel += 1;
+				break;
+			}
+		}
+		
+		System.out.printf("Thank You%n"
+				+ "Alcohol: %d%n"
+				+ "Gasoline: %d%n"
+				+ "Diesel: %d%n",
+				alcohol, gasoline, diesel);
+		
+		
+		sc.close();
+
+	}
+
+}
